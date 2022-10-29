f9057fd495b3dfacaf7f47e991aeaab3537b9c9a
Ginakira <ginakira@foxmail.com>
2022-10-29 22:45:20 +0800

    fix: repair failure on build test

--- src/test/java/com/pmrfansub/subflow/SubflowApplicationTests.java
+++ src/test/java/com/pmrfansub/subflow/SubflowApplicationTests.java
@@ -19,6 +19,9 @@ package com.pmrfansub.subflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class SubflowApplicationTests {
